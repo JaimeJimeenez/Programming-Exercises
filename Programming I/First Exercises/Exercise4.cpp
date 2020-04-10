614d7c53435352f504aca95da9e7085ed12aa4ac
Jaime Jiménez <55250735+Jimmyjimmenez@users.noreply.github.com>
2020-04-10 13:29:52 +0200

    Update Exercise4.cpp

--- Programming I/First Exercises/Exercise4.cpp
+++ Programming I/First Exercises/Exercise4.cpp
@@ -12,9 +12,10 @@ int main() {
     for (int i{ 0 }; i < imputSentence.size(); i++) {
         if (imputSentence.find("password") != -1) {
             password = imputSentence.substr(imputSentence.find("password") + 8, imputSentence.find(" ", imputSentence.find("password") + 1));
+            std::cout << "Password founded: " << password;
+            break;
         }
     }
-    std::cout << "Password founded: " << password;
     
     return 0;
 }
